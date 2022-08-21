064a7d846c95c2f6b2a9f577b460b51e5fe721ec
Igor Dragun <igor_dragun@bk.ru>
2022-08-21 13:58:49 +0400

    Add using metadata and tags

--- spec/lib/exchange_it/user_spec.rb
+++ spec/lib/exchange_it/user_spec.rb
@@ -5,7 +5,9 @@ RSpec.describe ExchangeIt::User do
   # let(:user) {ExchangeIt::User.new 'John', 'Doe'}
 
   # При указании в 'describe' имени класса далее внутри блока можно использовать указатель 'described_class'
-  let(:user) { described_class.new 'John', 'Doe' }
+  # При необходимости использования объекта в тестах с отличным параметром, например, именем,
+  # можно использовать метаданные
+  let(:user) { |example| described_class.new(example.metadata[:name] || 'John', 'Doe') }
 
   # Для описани теста можно использовать 'it', 'specify', 'example':
   # specify - если тестируем какой-то метод ('.' - метод класса, '#' - метод экземпляра класса);
@@ -16,15 +18,17 @@ RSpec.describe ExchangeIt::User do
     expect(user.name).to eq('John')
   end
 
-  it 'returns name as a string' do
-    user = described_class.new nil, 'Doe'
+  # В тесте, в котором необходимо использовать общий объект, но с отличным параметром, определяем метаданные (name)
+  it 'returns name as a string', name: nil do
     expect(user.name).to be_an_instance_of(String)
   end
 
-  it 'returns surname' do
+  # Тесты можно отмечать тэгами и вызывать только определенные тесты
+  it 'returns surname', :fast do
     expect(user.surname).to eq('Doe')
   end
 
+  # Важные тесты можно также отмечать, используя приставку 'f' - это аналогично добавлению тега 'focus'
   it 'returns surname as a string' do
     user = described_class.new 'John', nil
     expect(user.surname).to be_an_instance_of(String)
